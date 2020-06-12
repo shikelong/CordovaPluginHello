b7d5564d731a21cfd8366dd525dd6db4a3f3db73
RioShi <rio.shi@grapecity.com>
2020-06-12 14:35:09 +0900

    fix invalid action error

--- www/cordovaRioTest.js
+++ www/cordovaRioTest.js
@@ -1,5 +1,6 @@
 var exec = require('cordova/exec');
 
 exports.helloWorld = function(arg0, success, error){
-    exec(success, error, 'HelloWorld', 'helloWorld', [arg0]);
+    //callback success, callback error, service/class name, action name
+    exec(success, error, 'HelloWorld', 'helloworld', [arg0]);
 }
